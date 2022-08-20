cc2d3b21108d6a86379840baedd151dc7adf6b4a
brkyozdemir <berkayozdemir02@gmail.com>
2022-08-20 22:13:55 +0300

    remove timestamp from response

--- cmd/models/message.go
+++ cmd/models/message.go
@@ -4,5 +4,5 @@ type Message struct {
 	Sender    string `json:"sender"`
 	Receiver  string `json:"receiver"`
 	Message   string `json:"message"`
-	CreatedAt int64  `json:"created_at"`
+	CreatedAt int64  `json:"-"`
 }
